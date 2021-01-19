868329f99f1178cfa82c234eb8fa6fe9ab20d4b4
vodkaskywalker <vodkaskywalker@users.noreply.github.com>
2021-01-19 17:36:53 +0700

    Update github.sh

--- github.sh
+++ github.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 curl -X PATCH -H 'Accept: application/vnd.github.v3+json' \
--H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/bernhardfritz/pobfrontend/releases/27766214 \
+-H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/vodkaskywalker/pobfrontend/releases/36591114 \
 --data @<(cat <<-EOF
 { "tag_name":"PathOfBuilding",
 "target_commitish": "master",
 "name": "PathOfBuilding",
-"body": "build by Travis CI ${TR_TAG}",
+"body": "build by Travis CI ${TR_TAG} - Weekly",
 "draft": false, "prerelease": false }
 EOF
 )
