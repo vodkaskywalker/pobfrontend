13576960d490f6da173d1b501b3d487a906598ca
vodkaskywalker <vodkaskywalker@users.noreply.github.com>
2021-01-19 18:00:23 +0700

    Update github.sh

--- github.sh
+++ github.sh
@@ -6,7 +6,7 @@ curl -X PATCH -H 'Accept: application/vnd.github.v3+json' \
 { "tag_name":"PathOfBuilding",
 "target_commitish": "master",
 "name": "PathOfBuilding",
-"body": "build by Travis CI ${TR_TAG} - Weekly",
+"body": "build by Travis CI ${TR_TAG}",
 "draft": false, "prerelease": false }
 EOF
 )
